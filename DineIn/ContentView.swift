31ea4a8c36f4d044a3d9b2fc0384693a5050578e
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-10-30 22:25:57 +0530

    Initial Commit

---
+++ DineIn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DineIn
+//
+//  Created by Gokul Nair on 30/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
